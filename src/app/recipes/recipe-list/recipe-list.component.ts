794b4145c829c8b6a73e636f31449e733ee45f3b
Arupali Akash <akasharupali@gmail.com>
2024-04-01 12:11:20 +0530

    pushing a code

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.module';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[]=[
+    new Recipe('Biryani','Worlds fav food','https://media.istockphoto.com/id/1345624336/photo/chicken-biriyani.jpg?s=612x612&w=0&k=20&c=adU_N0P-1SKMQLZu5yu7aPknfLLgbViI8XILqLP92A4=')
+  ];
+  constructor() {
+
+  }
+  ngOnInit() {
+    
+  }
+
+}
